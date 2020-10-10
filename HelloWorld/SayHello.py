c9976e3b1623bea95d3af3846d0caf92dfa080b2
bfeng <bfeng@nltechdev.com>
2020-10-10 15:42:33 +0800

    daily build

--- HelloWorld/SayHello.py
+++ HelloWorld/SayHello.py
@@ -8,10 +8,17 @@ multiple lines comments"""
 
 a = 3
 b = 4
-c = math.e
+c = math.pi
 n = math.hypot(a, b)
-
+var2 = "Runoob"
 queue = deque()
 
-
+print(var2[-1::-1])
+print(n)
 print (c)
+print(queue)
+
+dict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
+
+#del(dict)
+print(dict)
